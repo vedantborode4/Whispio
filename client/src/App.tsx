0c9f377ca91bf16acc109215fe11933cefbb3e1f
Vedant <borodevedant4@gmail.com>
2025-07-29 22:52:45 +0530

    chore(setup): basic setup and add required dependencies

---
+++ client/src/App.tsx
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <div className="flex items-center h-screen justify-center">App</div>
+  )
+}
+
+export default App
---
+++ server/src/index.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
